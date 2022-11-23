07547a8793b4b0b10d24031bb6820238f41677f2
Piotr Buchman <kontakt@pbuchman.com>
2022-11-23 23:52:36 +0100

    Skeleton for the simulator-service

---
+++ simulator-service/src/main/java/com/pbuchman/simulatorservice/SimulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pbuchman.simulatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulatorServiceApplication.class, args);
+    }
+
+}
---
+++ simulator-service/src/test/java/com/pbuchman/simulatorservice/SimulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbuchman.simulatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
